6a4dff722fa58f5697f64423aef3415d943f4d48
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2026-09-01 18:57:53 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DeptDAOIntf intf = new DeptDAOImpl();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Department dept = intf.selectDept(40);
+        System.out.println("Department Number    : "+dept.getDepartmentNumber());
+        System.out.println("Department Name      : "+dept.getDepartmentName());
+        System.out.println("Departmenet location : "+dept.getLocation());
+
     }
 }
